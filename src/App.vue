ec3bef830e1c6857ae435d4a432db06eceeae597
liwen <243533657@qq.com>
2020-05-11 14:08:30 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 
 <template>
   <div id="app">
-  <img src="./assets/logo.png">
+<!--  <img src="./assets/logo.png">-->
     <router-view/>
-    <first></first>
+<!--    <first></first>-->
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import First from '@/components/views/First'
 import Second from '@/components/views/Second'
 
@@ -9,13 +8,13 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //     //   path: '/',
-    //     //   name: 'HelloWorld',
-    //     //   component: HelloWorld
-    //     // },
     {
-      path: '/',
+          path: '/',
+          name: 'First',
+          component: First
+        },
+    {
+      path: '/first',
       name: 'First',
       component: First
     },   {
